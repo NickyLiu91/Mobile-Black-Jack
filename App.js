4660c9d78aac5c8f5d71ae67aff4ae2caa8b50d8
Nicky <nickyliu91@gmail.com>
2019-09-28 02:26:43 -0400

    fixed so deck is passed to card component

--- App.js
+++ App.js
@@ -2,39 +2,32 @@ import React, {Component} from 'react';
 import Card from './Card.js';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-state = {
-  cards: []
-}
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 const allNumbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 const allSuites = ['Diamonds', "Clubs", "Hearts", "Spades"]
 
 const deck = []
 
-generateDeck = () => {
-  allNumbers.forEach(cardNumber => {
-    allSuites.forEach(cardSuit => {
-      deck.push({number: cardNumber, suit: cardSuit})
+export default class App extends Component {
+
+  state = {
+    cards: []
+  }
+
+  generateDeck = () => {
+    allNumbers.forEach(cardNumber => {
+      allSuites.forEach(cardSuit => {
+        deck.push({number: cardNumber, suit: cardSuit})
+      })
     })
-  })
-}
+  }
 
-componentDidMount(){
-  this.generateDeck()
-  this.setState({
-    cards: deck
-  })
-}
+  componentDidMount(){
+    this.generateDeck()
+    this.setState({
+      cards: deck
+    })
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
--- Card.js
+++ Card.js
@@ -1,27 +1,17 @@
 import React, { Component } from "react";
 // import {Platform} from 'react-native';
+import {Text, View} from 'react-native';
 
 export default class Card extends Component {
   render () {
     return (
-        {props.cards.length}
+      <Text>
+      {this.props.cards.length}
+      </Text>
     )
   }
 }
 
 // {Math.floor(Math.random() * Math.floor(props.cards.length))}
 
-// const Instructions = () => {
-//   return (
-//     '???????????'
-//   )
-// }
-
-// Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu'
-// })
-
-// export default Instructions;
+// export default Card;
